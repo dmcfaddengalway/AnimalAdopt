8b35d246d431633a3f6d3130640711f4508ef948
Daniel McFadden <dmcfaddengalway@gmail.com>
2019-05-23 19:53:33 +0100

    remove node

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
 
+import { TranslateService } from '@ngx-translate/core';
+import { InternalisationService } from '../app/services/internalisation/internalisation.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
   title = 'AnimalAdopt';
+  chosenLanguage = 'en';
+
+  constructor(protected translateService: TranslateService, protected internalisationService: InternalisationService) {
+    translateService.setDefaultLang('fr');
+    document.cookie = 'userAppLanguage=' + this.internalisationService.setAppLanguage();
+  }
+
+  public switchAppLanguage(languageChoice) {
+    this.chosenLanguage = languageChoice;
+  }
+
 }
